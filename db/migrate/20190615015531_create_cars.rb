2f193967bef7902be971f58c5e922b2b067c36ae
jimmy.clayton71@gmail.com <jimmyclay@Jimmys-MacBook-Pro-3.local>
2019-06-14 22:07:19 -0400

    initial commit

---
+++ db/migrate/20190615015531_create_cars.rb
@@ -0,0 +1,10 @@
+class CreateCars < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cars do |t|
+      t.stringmodel :make
+      t.integer :year
+
+      t.timestamps
+    end
+  end
+end
